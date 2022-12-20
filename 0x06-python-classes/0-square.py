46bced3cfb06724ac86fadfcbe2451d03ec0cc0b
DukeEthio <dukeethiopian@gmail.com>
2022-12-20 13:07:05 +0000

    0x06-python-classes all tascks

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+File: 0-square.py
+Desc: This module contains a single empty class defination.
+Author: Mesfin Tessema (Duke_Ethio)
+Date Created: Dec 20 2022
+"""
+
+
+class Square():
+    """
+    This is an empty class created for showing how to create
+    a class and it does nothing.
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+File: 0-square.py
+Desc: This module contains a single empty class defination.
+Author: Mesfin Tessema (Duke_Ethio)
+Date Created: Dec 20 2022
+"""
+
+class Square():
+    """
+    This class contains a defination of a single private object attribute
+    called size.
+    """
+    def __init__(self, size):
+        self.__size = size
