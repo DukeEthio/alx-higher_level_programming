45f7faf5f39e660974c74871b55ddc80c11879c1
DukeEthio <dukeethiopian@gmail.com>
2022-12-20 18:07:18 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 """
-File: 0-square.py
-Desc: This module contains a single empty class defination.
+File: 2-square.py
+Desc: This module contains a single class defination called Size.
 Author: Mesfin Tessema (Duke_Ethio)
 Date Created: Dec 20 2022
 """
+
 
 class Square():
     """
