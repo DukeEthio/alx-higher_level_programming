465c8f2540bd9bb3db09d8d1515e1e72161cc374
DukeEthio <dukeethiopian@gmail.com>
2022-12-20 18:30:42 +0000

    advanced tasks

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 """
-File: 0-square.py
+File: 100-singly_linked_list.py
 Desc: This module contains a single empty class defination.
 Author: Mesfin Tessema (Duke_Ethio)
 Date Created: Dec 20 2022
 """
+
 
 class Node():
     """
--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 """
-File: 0-square.py
+File: 103-magic_class.py
 Desc: This module contains a single empty class defination.
 Author: Mesfin Tessema (Duke_Ethio)
 Date Created: Dec 20 2022
 """
+
 
 import math
 
